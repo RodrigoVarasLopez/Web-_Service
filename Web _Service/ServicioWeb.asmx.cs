ddc24c7c714d36620fcd418ed21a5498e93e3f0b
Rodrigo Varas López <rodrigovaraslopez@gmail.com>
2016-02-16 21:47:45 +0100

    ultima

---
+++ Web _Service/ServicioWeb.asmx.cs
@@ -0,0 +1,39 @@
+﻿using System;
+using System.Collections;
+using System.Collections.Generic;
+using System.Data.SqlClient;
+using System.Linq;
+using System.Web;
+using System.Web.Services;
+using System.Runtime.Serialization;
+
+namespace Web__Service
+{
+    /// <summary>
+    /// Descripción breve de ServicioWeb
+    /// </summary>
+    [WebService(Namespace = "http://tempuri.org/")]
+    [WebServiceBinding(ConformsTo = WsiProfiles.BasicProfile1_1)]
+    [System.ComponentModel.ToolboxItem(false)]
+    // Para permitir que se llame a este servicio web desde un script, usando ASP.NET AJAX, quite la marca de comentario de la línea siguiente. 
+    // [System.Web.Script.Services.ScriptService]
+    public class ServicioWeb : System.Web.Services.WebService
+    {
+
+        [WebMethod]
+        public int autenticacion(string nombre, string contraseña)
+        {
+            Modelo.entidades.Usuario usuario = Modelo.consultas.isUsuario(nombre,contraseña);
+            if (usuario != null)
+            {
+                return 1;
+            }
+            return 0;
+        }
+        [WebMethod]
+        public Modelo.entidades.Obra consulta_Obras(string nombre)
+        {
+            return Modelo.consultas.isObraMovil(nombre);
+        }
+    }
+}
